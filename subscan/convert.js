58c408ae5ef66d94b3171791da7fd264043a60c5
Luke Schoen <ltfschoen@gmail.com>
2022-02-18 18:11:03 +0100

    fix indent

--- subscan/convert.js
+++ subscan/convert.js
@@ -6,49 +6,48 @@ const { encodeAddress } = require('@polkadot/util-crypto');
 function accountPublicKeyToSS58(accountPublicKey) {
   let accountSS58;
   let ss58FormatDataHighway = 33;
-	try {
-		accountSS58 = String(encodeAddress(`0x${accountPublicKey}`, ss58FormatDataHighway));
+  try {
+    accountSS58 = String(encodeAddress(`0x${accountPublicKey}`, ss58FormatDataHighway));
     console.log('encoded pub key to SS58: ', accountPublicKey, accountSS58);
-	} catch (e) {
+  } catch (e) {
     throw new Error(`Unable to convert public key hex to SS58 address {e}`);
-	}
+  }
 
   return accountSS58;
 }
 
 async function main () {
-    // const FILENAME = process.env.FILENAME;
-    const FILENAME = "datahighway.api.subscan.io-api-scan-accounts-2022-02-03-12:09-993000000-genesis-fixture";
+  const FILENAME = process.env.FILENAME;
 
-    // verify able to read data that was stored
-    let data, dataParsedJSON;
-    try {
-      data = fs.readFileSync(`${process.cwd()}/data/${FILENAME}.json`, 'utf8');
-      dataParsedJSON = JSON.parse(data);
-    } catch (err) {
-      console.log(`Error reading file: ${err}`);
-    }
-    // console.log('accounts available: ', dataParsedJSON["balances"].length);
+  // verify able to read data that was stored
+  let data, dataParsedJSON;
+  try {
+    data = fs.readFileSync(`${process.cwd()}/data/${FILENAME}.json`, 'utf8');
+    dataParsedJSON = JSON.parse(data);
+  } catch (err) {
+    console.log(`Error reading file: ${err}`);
+  }
+  // console.log('accounts available: ', dataParsedJSON["balances"].length);
 
-    let maxBalances = dataParsedJSON["balances"].length;
-    
-    let accountPublicKey;
-    let newAccount;
-    let ss58ForPublicKey;
-    let genesisObj = {
-      "balances": []
-    };
-    for (let accountIndex = 0; accountIndex <= maxBalances - 1; accountIndex++) {
-      accountPublicKey = dataParsedJSON["balances"][accountIndex][0];
-      // console.log('accountPublicKey: ', accountPublicKey);
-      ss58ForPublicKey = accountPublicKeyToSS58(accountPublicKey);
-      newAccount = [
-        ss58ForPublicKey,
-        dataParsedJSON["balances"][accountIndex][1],
-      ];
-      genesisObj["balances"].push(newAccount);
-    }
-    console.log("genesisObj: ", genesisObj);
+  let maxBalances = dataParsedJSON["balances"].length;
+  
+  let accountPublicKey;
+  let newAccount;
+  let ss58ForPublicKey;
+  let genesisObj = {
+    "balances": []
+  };
+  for (let accountIndex = 0; accountIndex <= maxBalances - 1; accountIndex++) {
+    accountPublicKey = dataParsedJSON["balances"][accountIndex][0];
+    // console.log('accountPublicKey: ', accountPublicKey);
+    ss58ForPublicKey = accountPublicKeyToSS58(accountPublicKey);
+    newAccount = [
+      ss58ForPublicKey,
+      dataParsedJSON["balances"][accountIndex][1],
+    ];
+    genesisObj["balances"].push(newAccount);
+  }
+  console.log("genesisObj: ", genesisObj);
 
   // serialize the modified data to JSON and store in file
   // format output indentation with 4 spaces
